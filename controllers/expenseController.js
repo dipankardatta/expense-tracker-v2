7abb2b52a85586e8bb8e31f1507d2549828cb844
Your Name <you@example.com>
2023-05-17 21:02:35 +0530

    Used transaction to update

--- controllers/expenseController.js
+++ controllers/expenseController.js
@@ -1,4 +1,6 @@
 const Expense = require('../models/expense');
+const User = require('../models/user')
+const sequelize = require('../util/database')
 
 
 const getExpenses = async (req, res) => {
@@ -12,26 +14,53 @@ const getExpenses = async (req, res) => {
 };
 
 const createExpense = async (req, res) => {
-  try {
-    const { name, amount } = req.body;
-    const expense = await Expense.create({ name, amount, userId: req.user.id });
-    res.json(expense);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: 'Internal server error' });
+  const t = await sequelize.transaction()
+  const { name, amount } = req.body;
+
+  if (amount == undefined || amount.length === 0){
+    return res.status(400).json({success:false, message:'Paramters Missing'})
   }
+  Expense.create({ name, amount, userId: req.user.id }).then(expense =>{
+    const totalExpense = Number(req.user.totalExp) + Number(amount)
+    console.log(totalExp)
+    User.update({
+      totalExp: totalExpense
+    },{
+      where: {id: req.user.id},
+      transaction :t
+    }).then(async() => {
+      await t.commit()
+      return res.status(200).json({expense: expense})
+    })
+    .catch(async(err) =>{
+      await t.rollback()
+      return res.status(500).json({success: false,error:err})
+    })
+  }).catch(async(err) =>{
+    await t.rollback()
+    return res.status(500).json({success: false,error:err})
+  })
+
 };
 
 const deleteExpense = async (req, res) => {
   try {
     const { id } = req.params;
-    await Expense.destroy({ where: { id, userId: req.user.id } });
+
+    const t = await sequelize.transaction();
+
+    await Expense.destroy({ where: { id, userId: req.user.id }, transaction: t });
+
+    await t.commit();
+
     res.sendStatus(204);
   } catch (error) {
     console.log(error);
+    await t.rollback();
     res.status(500).json({ error: 'Internal server error' });
   }
 };
+
 
 module.exports = {
   getExpenses,
