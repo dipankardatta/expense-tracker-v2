04c3f4d5ca61df677358d6ef6634a9a5e91773c3
Your Name <you@example.com>
2023-05-20 18:24:03 +0530

    added IAM user and S3 features

--- controllers/expenseController.js
+++ controllers/expenseController.js
@@ -1,7 +1,54 @@
 const Expense = require('../models/expense');
 const User = require('../models/user')
 const sequelize = require('../util/database')
+const AWS = require('aws-sdk')
+const Userservices = require('../services/userservices')
 
+function uploadToS3(data,filename){
+    const BUCKET_NAME = 'expensetrackss';
+    const IAM_USER_KEY = 'AKIASH3WOREHVTOHVWVW';
+    const IAM_USER_SECRET='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';
+
+    let s3bucket = new AWS.S3({
+      accessKeyId : IAM_USER_KEY,
+      secretAccessKey: IAM_USER_SECRET,
+    })
+       var params = {
+        Bucket: BUCKET_NAME,
+        Key: filename,
+        Body: data,
+        ACL: 'public-read'
+      }
+      return new Promise ((resolve,reject)=>{
+        s3bucket.upload(params,(err,s3response)=>{
+          if(err){
+            console.log('Something went wrong',err)
+            reject(err)
+          }
+          else{
+            // console.log('success', s3response)
+            resolve( s3response.Location)
+          }
+        })
+      })
+      
+}
+
+const downloadExpense = async (req,res) =>{
+  try {
+    const expenses = await Userservices.getExpenses(req)
+    const stringifiedExpenses = JSON.stringify(expenses);
+    const userId = req.user.id
+    const filename = `Expense${userId}/${new Date()}.txt`;
+    const fileURL = await uploadToS3( stringifiedExpenses,filename);
+    res.status(200).json({fileURL,success: true})
+  } catch(err){
+    console.log(err)
+    res.status(500).json({fileURL:"",success:false,err:null})
+  }
+    
+    
+}
 
 const getExpenses = async (req, res) => {
   
@@ -95,4 +142,5 @@ module.exports = {
   getExpenses,
   createExpense,
   deleteExpense,
+  downloadExpense
 };
--- routes/expense.js
+++ routes/expense.js
@@ -6,10 +6,15 @@ const userauthentication = require('../middleware/auth');
 // GET all expenses
 router.get('/', userauthentication.authenticate , expenseController.getExpenses);
 
+//download expenses
+router.get('/download', userauthentication.authenticate, expenseController.downloadExpense)
+
 // POST a new expense
 router.post('/',userauthentication.authenticate, expenseController.createExpense);
 
 // DELETE an expense by ID
 router.delete('/:id', userauthentication.authenticate,expenseController.deleteExpense);
+
+
 
 module.exports = router; 
